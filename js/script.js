75f91500074279a3740eb377cf7d98a7e446e519
Mugilan <mugilancodes@gmail.com>
2020-06-22 11:35:12 +0530

    Add Color

--- js/script.js
+++ js/script.js
@@ -5,14 +5,22 @@ const clearGrid = () => {
   gridContainer__div.innerHTML = '';
 };
 
+//! Maximum size is 120
 const createGrid = (size = 16) => {
   clearGrid();
+
+  size = size < 1 ? 1 : size > 120 ? 120 : size;
+  console.log({ size });
   root.style.setProperty('--grid-size', size);
+
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       const div = document.createElement('div');
       div.setAttribute('class', 'grid-item');
       div.setAttribute('id', `${i}X${j}`);
+      div.addEventListener('mouseover', (e) =>
+        e.target.classList.add('passed-over')
+      );
       gridContainer__div.appendChild(div);
     }
   }
